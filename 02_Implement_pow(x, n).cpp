dddb00835ab854473bd97f8d860e0ca2b696d872
Shani yadav <shaniyadav4518@gmail.com>
2025-11-22 17:43:29 +0530

    Update 02_Implement_pow(x, n).cpp

--- 02_Implement_pow(x, n).cpp
+++ 02_Implement_pow(x, n).cpp
@@ -1,18 +1,6 @@
 class Solution {
 public:
-    int reverse(int x) {
-        int num;
-        long long rev=0;
-        while(x!=0){
-            num=x%10;
-            rev=(rev*10)+num;
-            x=x/10;}
-             if (rev > INT_MAX || rev < INT_MIN) {
-                return 0;
-            }
-        
-
-        return int(rev);
-        
+    double myPow(double x, int n) {
+      return pow(x,n);
     }
 };
